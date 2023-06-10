d856b6bbc22d218100a2fc3a995098d030aebd51
Jonas <p429344@gmail.com>
2023-06-11 00:55:34 +0200

    :sparkles: add: scheduled events events

---
+++ src/events/events/GuildScheduledEventDelete.ts
@@ -0,0 +1,32 @@
+import BaseClient from "@structures/BaseClient";
+import { EmbedBuilder } from "discord.js";
+import moment from "moment";
+
+export default class {
+    public client: BaseClient;
+
+    constructor(client: BaseClient) {
+        this.client = client;
+    }
+
+    async dispatch(scheduledEvent: any): Promise<void> {
+        if(!scheduledEvent || !scheduledEvent.guild) return;
+        const { guild } = scheduledEvent;
+
+        const properties: Array<string> = [];
+        if(scheduledEvent.name) properties.push(this.client.emotes.edit + " Name: " + scheduledEvent.name);
+        if(scheduledEvent.id) properties.push(this.client.emotes.id + " ID: " + scheduledEvent.id);
+        if(scheduledEvent.description) properties.push(this.client.emotes.text + " Beschreibung: " + scheduledEvent.description);
+        if(scheduledEvent.scheduledStartTimestamp) properties.push(this.client.emotes.reminder + " Startzeit: " + moment(scheduledEvent.scheduledStartTimestamp).format("DD.MM.YYYY HH:mm"));
+        if(scheduledEvent.scheduledEndTimestamp) properties.push(this.client.emotes.reminder + " Endzeit: " + moment(scheduledEvent.scheduledEndTimestamp).format("DD.MM.YYYY HH:mm"));
+
+        const scheduledEventLogMessage: string =
+            properties.join("\n");
+
+        const scheduledEventLogEmbed: EmbedBuilder = this.client.createEmbed(scheduledEventLogMessage, null, "error");
+        scheduledEventLogEmbed.setTitle(this.client.emotes.events.event.delete + "Event gelöscht");
+        scheduledEventLogEmbed.setThumbnail(guild.iconURL());
+
+        await guild.logAction(scheduledEventLogEmbed, "guild");
+    }
+}
---
+++ src/events/events/GuildScheduledEventUpdate.ts
@@ -0,0 +1,32 @@
+import BaseClient from "@structures/BaseClient";
+import { EmbedBuilder } from "discord.js";
+import moment from "moment";
+
+export default class {
+    public client: BaseClient;
+
+    constructor(client: BaseClient) {
+        this.client = client;
+    }
+
+    async dispatch(oldScheduledEvent: any, newScheduledEvent: any): Promise<any> {
+        if(!oldScheduledEvent || !newScheduledEvent || !newScheduledEvent.guild) return;
+        const { guild } = newScheduledEvent;
+
+        const properties: Array<string> = [];
+        if(oldScheduledEvent.name !== newScheduledEvent.name) properties.push(this.client.emotes.edit + " Name: ~~" + oldScheduledEvent.name + "~~ **" + newScheduledEvent.name + "**");
+        if(oldScheduledEvent.description !== newScheduledEvent.description) properties.push(this.client.emotes.text + " Beschreibung: ~~" + oldScheduledEvent.description + "~~ **" + newScheduledEvent.description + "**");
+        if(oldScheduledEvent.scheduledStartTimestamp !== newScheduledEvent.scheduledStartTimestamp) properties.push(this.client.emotes.reminder + " Startzeit: ~~" + (oldScheduledEvent.scheduledStartTimestamp ? moment(oldScheduledEvent.scheduledStartTimestamp).format("DD.MM.YYYY HH:mm") : "/") + "~~ **" + (newScheduledEvent.scheduledStartTimestamp ? moment(newScheduledEvent.scheduledStartTimestamp).format("DD.MM.YYYY HH:mm") : "/") + "**");
+        if(oldScheduledEvent.scheduledEndTimestamp !== newScheduledEvent.scheduledEndTimestamp) properties.push(this.client.emotes.reminder + " Startzeit: ~~" + (oldScheduledEvent.scheduledEndTimestamp ? moment(oldScheduledEvent.scheduledEndTimestamp).format("DD.MM.YYYY HH:mm") : "/") + "~~ **" + (newScheduledEvent.scheduledEndTimestamp ? moment(newScheduledEvent.scheduledEndTimestamp).format("DD.MM.YYYY HH:mm") : "/") + "**");
+        if(properties.length < 1) return;
+
+        const scheduledEventLogMessage: string =
+            properties.join("\n");
+
+        const scheduledEventLogEmbed: EmbedBuilder = this.client.createEmbed(scheduledEventLogMessage, null, "warning");
+        scheduledEventLogEmbed.setTitle(this.client.emotes.events.event.update + "Event bearbeitet");
+        scheduledEventLogEmbed.setThumbnail(guild.iconURL());
+
+        await guild.logAction(scheduledEventLogEmbed, "guild");
+    }
+}
