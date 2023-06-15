e29fdc9dcc3c5d60fb6da27cc172bc33dc5326af
Jonas <p429344@gmail.com>
2023-06-16 00:03:43 +0200

    :sparkles: add: member

---
+++ src/events/member/GuildMemberRemove.ts
@@ -0,0 +1,105 @@
+import { EmbedBuilder } from "discord.js";
+import BaseClient from "@structures/BaseClient";
+
+export default class
+{
+    public client: BaseClient;
+
+    constructor(client: BaseClient)
+    {
+        this.client = client;
+    }
+
+    async dispatch(member: any): Promise<any>
+    {
+        if(!member || !member.id || !member.guild) return;
+        const { guild } = member;
+
+        const guildData = await this.client.findOrCreateGuild(guild.id);
+        const memberData = await this.client.findOrCreateMember(member.id, guild.id);
+
+        /* Update invite cache */
+        if(memberData.inviteUsed){
+            const invite: any = await guild.invites.fetch(memberData.inviteUsed).catch((e: any): void => {});
+            if(invite){
+                const inviterData = await this.client.findOrCreateMember(invite.inviterId, guild.id);
+                if(!inviterData.invites) inviterData.invites = [];
+                inviterData.invites.find((i: any): boolean => i.code === invite.code).left++;
+                inviterData.markModified("invites");
+                await inviterData.save();
+            }
+        }
+
+        /* Send log */
+        const memberLeaveText: string =
+            this.client.emotes.edit + " Anzeigename: " + member.user.displayName + "\n" +
+            this.client.emotes.user + " Nutzername: " + member.user.username + "\n" +
+            this.client.emotes.id + " ID: " + member.id;
+
+        const memberLeaveEmbed: EmbedBuilder = this.client.createEmbed(memberLeaveText, null, "error");
+        memberLeaveEmbed.setTitle(this.client.emotes.events.member.ban + "Mitglied hat den Server verlassen");
+        memberLeaveEmbed.setThumbnail(member.user.displayAvatarURL());
+
+        await guild.logAction(memberLeaveEmbed, "member");
+
+        /* Send farewell message */
+        if(guildData.settings?.farewell.enabled){
+            function parseMessage(str: string): string
+            {
+                return str
+                    .replaceAll(/{user}/g, member)
+                    .replaceAll(/{user:username}/g, member.user.username)
+                    .replaceAll(/{user:tag}/g, member.user.tag)
+                    .replaceAll(/{user:discriminator}/g, member.user.discriminator)
+                    .replaceAll(/{user:id}/g, member.user.id)
+                    .replaceAll(/{server:name}/g, guild.name)
+                    .replaceAll(/{server:id}/g, guild.id)
+                    .replaceAll(/{server:membercount}/g, guild.memberCount)
+                    .replaceAll(/{newline}/g, "\n");
+            }
+
+            const farewellMessage: string = parseMessage(guildData.settings.farewell.message);
+            const farewellChannel: any = guild.channels.cache.get(guildData.settings.farewell.channel) || await guild.channels.fetch(guildData.settings.farewell.channel)
+                .catch((e: any): void => {
+                    const errorText: string =
+                        this.client.emotes.user + " Mitglied: " + member.user.username + "\n" +
+                        this.client.emotes.arrow + " Kanal: " + guildData.settings.welcome.channel;
+
+                    const errorEmbed: EmbedBuilder = this.client.createEmbed(errorText, null, "error");
+                    errorEmbed.setTitle(this.client.emotes.error + " Verabschiedungsnachricht fehlgeschlagen");
+                    errorEmbed.setThumbnail(member.user.displayAvatarURL());
+
+                    guild.logAction(errorEmbed, "guild");
+                });
+
+            if(guildData.settings.farewell.type === "embed"){
+                const farewellEmbed: EmbedBuilder = this.client.createEmbed(farewellMessage, null, "normal");
+                farewellEmbed.setThumbnail(member.user.displayAvatarURL());
+                return farewellChannel.send({ embeds: [farewellEmbed] }).catch((e: any): void => {
+                    const errorText: string =
+                        this.client.emotes.user + " Mitglied: " + member.user.username + "\n" +
+                        this.client.emotes.arrow + " Kanal: " + farewellChannel.toString();
+
+                    const errorEmbed: EmbedBuilder = this.client.createEmbed(errorText, null, "error");
+                    errorEmbed.setTitle(this.client.emotes.error + " Verabschiedungsnachricht fehlgeschlagen");
+                    errorEmbed.setThumbnail(member.user.displayAvatarURL());
+
+                    return guild.logAction(errorEmbed, "guild");
+                });
+            }else if(guildData.settings.welcome.type === "text"){
+                return farewellChannel.send({ content: farewellMessage })
+                    .catch((e: any): void => {
+                        const errorText: string =
+                            this.client.emotes.user + " Mitglied: " + member.user.username + "\n" +
+                            this.client.emotes.arrow + " Kanal: " + farewellChannel.toString();
+
+                        const errorEmbed: EmbedBuilder = this.client.createEmbed(errorText, null, "error");
+                        errorEmbed.setTitle(this.client.emotes.error + " Verabschiedungsnachricht fehlgeschlagen");
+                        errorEmbed.setThumbnail(member.user.displayAvatarURL());
+
+                        return guild.logAction(errorEmbed, "guild");
+                    })
+            }
+        }
+    }
+}
---
+++ src/events/member/GuildMemberUpdate.ts
@@ -0,0 +1,43 @@
+import { EmbedBuilder } from "discord.js";
+import BaseClient from "@structures/BaseClient";
+
+export default class
+{
+    public client: BaseClient;
+
+    constructor(client: BaseClient)
+    {
+        this.client = client;
+    }
+
+    async dispatch(oldMember: any, newMember: any): Promise<any>
+    {
+        if(oldMember.pending && !newMember.pending) this.client.emit("guildMemberAdd", newMember);
+        if(!oldMember || !newMember || !newMember.guild || oldMember.partial) return;
+
+        const { guild } = newMember;
+        if(!guild.members.cache.find((m: any): boolean => m.id === oldMember.id)) return;
+
+        const properties: Array<string> = [];
+
+        if(oldMember.displayName !== newMember.displayName) properties.push(this.client.emotes.edit + " Anzeigename: ~~" + oldMember.displayName + "~~ **" + newMember.displayName + "**");
+
+        newMember.roles.cache.forEach((role: any): void => {
+            if(!oldMember.roles.cache.has(role.id)) properties.push(this.client.emotes.events.role.create + " Rolle hinzugefügt: " + role.toString());
+        });
+
+        oldMember.roles.cache.forEach((role: any): void => {
+            if(!newMember.roles.cache.has(role.id)) properties.push(this.client.emotes.events.role.delete + " Rolle entfernt: " + role.toString());
+        });
+        if(properties.length < 1) return;
+
+        const memberUpdateText: string =
+            properties.join("\n");
+
+        const memberUpdateEmbed: EmbedBuilder = this.client.createEmbed(memberUpdateText, null, "warning");
+        memberUpdateEmbed.setTitle(this.client.emotes.events.member.update + " " + newMember.user.username + " wurde aktualisiert");
+        memberUpdateEmbed.setThumbnail(newMember.user.displayAvatarURL());
+
+        await guild.logAction(memberUpdateEmbed, "member");
+    }
+}
