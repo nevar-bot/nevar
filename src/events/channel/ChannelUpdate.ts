9cee0fed24d291c39566aa12c3a7e3f371ea3a51
Jonas <p429344@gmail.com>
2023-06-22 22:57:51 +0200

    :fire: improve channel update event

--- src/events/channel/ChannelUpdate.ts
+++ src/events/channel/ChannelUpdate.ts
@@ -16,16 +16,17 @@ export default class
 
         const properties: Array<string> = [];
         if(oldChannel.name !== newChannel.name) properties.push(this.client.emotes.edit + " Name: ~~" + oldChannel.name + "~~ **" + newChannel.name + "**");
-        if(oldChannel.topic !== newChannel.topic) properties.push(this.client.emotes.quotes + " Thema: ~~" + (oldChannel.topic ? oldChannel.topic : "/") + "~~ **" + (newChannel.topic ? newChannel.topic : "/") + "**");
+        if(oldChannel.topic !== newChannel.topic) properties.push(this.client.emotes.quotes + " Thema: ~~" + (oldChannel.topic ? oldChannel.topic : "Kein Thema") + "~~ **" + (newChannel.topic ? newChannel.topic : "Kein Thema") + "**");
         if(oldChannel.nsfw !== newChannel.nsfw) properties.push(this.client.emotes.underage + " Altersbegrenzung: ~~" + (oldChannel.nsfw ? "aktiv" : "inaktiv") + "~~ **" + (newChannel.nsfw ? "aktiv" : "inaktiv") + "**");
         if(oldChannel.parentId !== newChannel.parentId) properties.push(this.client.emotes.list + " Kategorie: ~~" + (oldChannel.parent?.name ? oldChannel.parent.name : "/")  + "~~ **" + (newChannel.parent?.name ? newChannel.parent.name : "/") + "**");
-        if(oldChannel.rateLimitPerUser !== newChannel.rateLimitPerUser) properties.push(this.client.emotes.timeout + " Slow-Modus: ~~" + (oldChannel.rateLimitPerUser ? oldChannel.rateLimitPerUser + " Sekunde(n)" : "/") + "~~ **" + (newChannel.rateLimitPerUser ? newChannel.rateLimitPerUser + " Sekunde(n)" : "/") + "**");
+        if(oldChannel.rateLimitPerUser !== newChannel.rateLimitPerUser) properties.push(this.client.emotes.timeout + " Slow-Modus: ~~" + (oldChannel.rateLimitPerUser ? oldChannel.rateLimitPerUser + " Sekunde(n)" : "Kein Limit") + "~~ **" + (newChannel.rateLimitPerUser ? newChannel.rateLimitPerUser + " Sekunde(n)" : "Kein Limit") + "**");
         if(oldChannel.bitrate !== newChannel.bitrate) properties.push(this.client.emotes.latency.good + " Bitrate: ~~" + oldChannel.bitrate/1000 + "kbps~~ **" + newChannel.bitrate/1000 + "kbps**");
         if(oldChannel.userLimit !== newChannel.userLimit) properties.push(this.client.emotes.users + " Userlimit: ~~" + (oldChannel.userLimit === 0 ? "unbegrenzt" : oldChannel.userLimit) + "~~ **" + (newChannel.userLimit === 0 ? "unbegrenzt" : newChannel.userLimit) + "**");
         if(oldChannel.videoQualityMode !== newChannel.videoQualityMode) properties.push(this.client.emotes.monitor + " Videoqualität: ~~" + (oldChannel.videoQualityMode === 1 ? "automatisch" : "720p") + "~~ **" + (newChannel.videoQualityMode === 1 ? "automatisch" : "720p") + "**");
         if(properties.length < 1) return;
 
         const channelLogMessage: string =
+            this.client.emotes.channel + " Kanal: " + newChannel.toString() + "\n" +
             properties.join("\n");
 
         const channelLogEmbed: EmbedBuilder = this.client.createEmbed(channelLogMessage, null, "warning");
