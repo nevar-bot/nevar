103b89ff8efa84b62ac7ca85c4ce88e8c568dbf5
Jonas <p429344@gmail.com>
2023-06-23 03:08:36 +0200

    :sparkles: add: warn event

---
+++ src/events/Warn.ts
@@ -0,0 +1,14 @@
+import BaseClient from "@structures/BaseClient";
+
+export default class {
+    private client: BaseClient;
+
+    public constructor(client: BaseClient) {
+        this.client = client;
+    }
+
+    public async dispatch(e: any): Promise<void> {
+        console.warn(e);
+        await this.client.alert(e, "warning");
+    }
+}
