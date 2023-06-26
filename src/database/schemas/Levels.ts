56935370dd484f0ef22db40d35628ecf34d4afd9
Jonas <p429344@gmail.com>
2023-06-26 21:21:45 +0200

    :art: run npm lint

--- src/database/schemas/Levels.ts
+++ src/database/schemas/Levels.ts
@@ -2,11 +2,11 @@ import * as mongoose from "mongoose";
 import { Model } from "mongoose";
 
 const Schema = new mongoose.Schema({
-    userID: { type: String },
-    guildID: { type: String },
-    xp: { type: Number, default: 0 },
-    level: { type: Number, default: 0 },
-    lastUpdated: { type: Number, default: new Date() }
+	userID: { type: String },
+	guildID: { type: String },
+	xp: { type: Number, default: 0 },
+	level: { type: Number, default: 0 },
+	lastUpdated: { type: Number, default: new Date() }
 });
 
 const Levels: Model<any> = mongoose.model("Levels", Schema);
