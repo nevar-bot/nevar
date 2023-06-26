eba9a866d67e582d476230330d2bcf2f4754b5e4
Jonas <p429344@gmail.com>
2023-06-26 17:51:49 +0200

    :sparkles: add topic command

---
+++ src/commands/fun/topic.ts
@@ -0,0 +1,34 @@
+import BaseCommand from "@structures/BaseCommand";
+import BaseClient from "@structures/BaseClient";
+import { SlashCommandBuilder } from "discord.js";
+import fs from "fs"
+
+export default class TopicCommand extends BaseCommand {
+    public constructor(client: BaseClient){
+        super(client, {
+            name: "topic",
+            description: "Sendet ein zufälliges Thema für eine Unterhaltung",
+            cooldown: 1000,
+            dirname: __dirname,
+            slashCommand: {
+                addCommand: true,
+                data:
+                    new SlashCommandBuilder()
+            }
+        });
+    }
+
+    private interaction: any;
+
+    public async dispatch(interaction: any, data: any): Promise<void> {
+        this.interaction = interaction;
+        return await this.getTopic();
+    }
+
+    private async getTopic(): Promise<void> {
+        const json = JSON.parse(String(fs.readFileSync(('./assets/topics.json'))));
+        const topics: any[] = Object.values(json);
+
+        return this.interaction.followUp({ content: topics[Math.floor(Math.random() * topics.length)] });
+    }
+}
