5fa4cd831789fd159e2f0adc587afdad657f7588
Jonas <p429344@gmail.com>
2023-06-27 20:20:47 +0200

    :art: run npm lint

--- src/commands/minigames/findemoji.ts
+++ src/commands/minigames/findemoji.ts
@@ -3,110 +3,122 @@ import BaseClient from "@structures/BaseClient";
 import BaseGame from "@structures/BaseGame";
 import { SlashCommandBuilder, ActionRowBuilder, EmbedBuilder, ButtonBuilder } from "discord.js";
 
-export default class FindemojiCommand extends BaseCommand {
-    public constructor(client: BaseClient) {
-        super(client, {
-            name: "findemoji",
-            description: "Du musst dir die Reihenfolge acht verschiedener Emojis merken, und den Richtigen wählen",
-            cooldown: 1000,
-            dirname: __dirname,
-            slashCommand: {
-                addCommand: true,
-                data: new SlashCommandBuilder()
-            }
-        });
-    }
+export default class FindemojiCommand extends BaseCommand
+{
+	public constructor(client: BaseClient)
+	{
+		super(client, {
+			name: "findemoji",
+			description: "Du musst dir die Reihenfolge acht verschiedener Emojis merken, und den Richtigen wählen",
+			cooldown: 1000,
+			dirname: __dirname,
+			slashCommand: {
+				addCommand: true,
+				data: new SlashCommandBuilder()
+			}
+		});
+	}
 
-    private interaction: any;
+	private interaction: any;
 
-    public async dispatch(interaction: any, data: any): Promise<void> {
-        this.interaction = interaction;
-        await this.startGame();
-    }
+	public async dispatch(interaction: any, data: any): Promise<void>
+	{
+		this.interaction = interaction;
+		await this.startGame();
+	}
 
-    private async startGame(): Promise<void> {
-        const game: FindemojiGame = new FindemojiGame({
-            interaction: this.interaction,
-            client: this.client
-        });
-        await game.startGame();
-    }
+	private async startGame(): Promise<void>
+	{
+		const game: FindemojiGame = new FindemojiGame({
+			interaction: this.interaction,
+			client: this.client
+		});
+		await game.startGame();
+	}
 }
 
-class FindemojiGame extends BaseGame {
+class FindemojiGame extends BaseGame
+{
 
-    public emojis: string[];
-    public selected: any;
-    public emoji: any;
+	public emojis: string[];
+	public selected: any;
+	public emoji: any;
 
-    public constructor(options: any = {}) {
-        super(options);
-        this.emojis = ['🍉', '🍇', '🍊', '🍋', '🥭', '🍎', '🍏', '🥝', '🥥', '🍓', '🍒'];
-        this.selected = null;
-        this.emoji = null;
-    }
+	public constructor(options: any = {})
+	{
+		super(options);
+		this.emojis = ['🍉', '🍇', '🍊', '🍋', '🥭', '🍎', '🍏', '🥝', '🥥', '🍓', '🍒'];
+		this.selected = null;
+		this.emoji = null;
+	}
 
-    public async startGame(): Promise<void> {
-        await this.interaction.deferReply().catch((e: any): void => {});
+	public async startGame(): Promise<void>
+	{
+		await this.interaction.deferReply().catch((e: any): void => { });
 
-        this.emojis = this.shuffleArray(this.emojis).slice(0, 8);
-        this.emoji = this.emojis[Math.floor(Math.random() * this.emojis.length)];
-
-
-        const findEmojiEmbed: EmbedBuilder = this.client.createEmbed("Du hast 5 Sekunden, um dir die Emojis in richtiger Reihenfolge zu merken!", "arrow", "normal");
-        findEmojiEmbed.setThumbnail(this.client.user!.displayAvatarURL());
-
-        const msg: any = await this.sendMessage({ embeds: [findEmojiEmbed], components: this.getComponents(true) });
-
-        const timeoutCallback: any = async (): Promise<void> => {
-            findEmojiEmbed.setDescription("Finde den " + this.emoji + " Emoji, bevor die Zeit abläuft");
-            await msg.edit({ embeds: [findEmojiEmbed], components: this.getComponents(false) });
-            const emojiCollector = msg.createMessageComponentCollector({ filter: (btn: any): boolean => btn.user.id === this.interaction.user.id, idle: 30000 });
-
-            emojiCollector.on('collect', async (btn: any): Promise<any> => {
-                await btn.deferUpdate().catch((e: any): void => {});
-                this.selected = this.emojis[parseInt(btn.customId.split('_')[1])];
-                return emojiCollector.stop();
-            });
-
-            emojiCollector.on('end', async (_: any, reason: any): Promise<any> => {
-                if (reason === 'idle' || reason === 'user') return this.endGame(msg, (reason === 'user'));
-            });
-        };
-        setTimeout(timeoutCallback, 5000);
-    }
+		this.emojis = this.shuffleArray(this.emojis).slice(0, 8);
+		this.emoji = this.emojis[Math.floor(Math.random() * this.emojis.length)];
 
 
-    private endGame(msg: any, result: any) {
-        const resultMessage: "win"|"lose" = (this.selected === this.emoji) ? "win" : "lose";
-        if (!result) this.selected = this.emoji;
+		const findEmojiEmbed: EmbedBuilder = this.client.createEmbed("Du hast 5 Sekunden, um dir die Emojis in richtiger Reihenfolge zu merken!", "arrow", "normal");
+		findEmojiEmbed.setThumbnail(this.client.user!.displayAvatarURL());
 
-        let finalMessage: string;
-        if(resultMessage === "win"){
-            finalMessage = "Du hast den richtigen Emoji ausgewählt. {0}";
-        }else{
-            finalMessage = "Du hast den falschen Emoji ausgewählt. {0}";
-        }
+		const msg: any = await this.sendMessage({ embeds: [findEmojiEmbed], components: this.getComponents(true) });
 
-        const gameOverEmbed: EmbedBuilder = this.client.createEmbed(finalMessage, "arrow", "normal", this.emoji);
-        gameOverEmbed.setThumbnail(this.client.user!.displayAvatarURL());
+		const timeoutCallback: any = async (): Promise<void> =>
+		{
+			findEmojiEmbed.setDescription("Finde den " + this.emoji + " Emoji, bevor die Zeit abläuft");
+			await msg.edit({ embeds: [findEmojiEmbed], components: this.getComponents(false) });
+			const emojiCollector = msg.createMessageComponentCollector({ filter: (btn: any): boolean => btn.user.id === this.interaction.user.id, idle: 30000 });
 
-        return msg.edit({ embeds: [gameOverEmbed], components: this.disableButtons(this.getComponents(true)) });
-    }
+			emojiCollector.on('collect', async (btn: any): Promise<any> =>
+			{
+				await btn.deferUpdate().catch((e: any): void => { });
+				this.selected = this.emojis[parseInt(btn.customId.split('_')[1])];
+				return emojiCollector.stop();
+			});
+
+			emojiCollector.on('end', async (_: any, reason: any): Promise<any> =>
+			{
+				if (reason === 'idle' || reason === 'user') return this.endGame(msg, (reason === 'user'));
+			});
+		};
+		setTimeout(timeoutCallback, 5000);
+	}
 
 
-    private getComponents(showEmoji: any): any {
-        const components: any[] = [];
-        for (let x: number = 0; x < 2; x++) {
-            const row: any = new ActionRowBuilder();
-            for (let y: number = 0; y < 4; y++) {
-                const buttonEmoji: string = this.emojis[x * 4 + y];
+	private endGame(msg: any, result: any)
+	{
+		const resultMessage: "win" | "lose" = (this.selected === this.emoji) ? "win" : "lose";
+		if (!result) this.selected = this.emoji;
 
-                const btn: ButtonBuilder = this.client.createButton("findEmoji_" + (x*4 + y), "\u200b", (buttonEmoji === this.selected ? (this.selected === this.emoji ? 'Success' : 'Danger') : "Primary"), (showEmoji ? buttonEmoji : null));
-                row.addComponents(btn);
-            }
-            components.push(row);
-        }
-        return components;
-    }
+		let finalMessage: string;
+		if (resultMessage === "win") {
+			finalMessage = "Du hast den richtigen Emoji ausgewählt. {0}";
+		} else {
+			finalMessage = "Du hast den falschen Emoji ausgewählt. {0}";
+		}
+
+		const gameOverEmbed: EmbedBuilder = this.client.createEmbed(finalMessage, "arrow", "normal", this.emoji);
+		gameOverEmbed.setThumbnail(this.client.user!.displayAvatarURL());
+
+		return msg.edit({ embeds: [gameOverEmbed], components: this.disableButtons(this.getComponents(true)) });
+	}
+
+
+	private getComponents(showEmoji: any): any
+	{
+		const components: any[] = [];
+		for (let x: number = 0; x < 2; x++) {
+			const row: any = new ActionRowBuilder();
+			for (let y: number = 0; y < 4; y++) {
+				const buttonEmoji: string = this.emojis[x * 4 + y];
+
+				const btn: ButtonBuilder = this.client.createButton("findEmoji_" + (x * 4 + y), "\u200b", (buttonEmoji === this.selected ? (this.selected === this.emoji ? 'Success' : 'Danger') : "Primary"), (showEmoji ? buttonEmoji : null));
+				row.addComponents(btn);
+			}
+			components.push(row);
+		}
+		return components;
+	}
 }
--- src/structures/BaseGame.ts
+++ src/structures/BaseGame.ts
@@ -1,36 +1,41 @@
 import BaseClient from "@structures/BaseClient";
 
-export default class BaseGame {
-    protected options: any;
-    protected client: BaseClient;
-    protected interaction: any;
+export default class BaseGame
+{
+	protected options: any;
+	protected client: BaseClient;
+	protected interaction: any;
 
-    protected constructor(options: any = {}) {
-        this.options = options;
-        this.client = options.client;
-        this.interaction = options.interaction;
-    }
+	protected constructor(options: any = {})
+	{
+		this.options = options;
+		this.client = options.client;
+		this.interaction = options.interaction;
+	}
 
-    protected async sendMessage(content: any): Promise<any> {
-        return await this.interaction.editReply(content);
-    }
+	protected async sendMessage(content: any): Promise<any>
+	{
+		return await this.interaction.editReply(content);
+	}
 
-    protected shuffleArray(array: any[]): any[] {
-        for (let i: number = array.length - 1; i > 0; i--) {
-            const j: number = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+	protected shuffleArray(array: any[]): any[]
+	{
+		for (let i: number = array.length - 1; i > 0; i--) {
+			const j: number = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
 
-        return array;
-    }
+		return array;
+	}
 
-    protected disableButtons(components: any): any {
-        for (const element of components) {
-            for (const component of element.components) {
-                component.data.disabled = true;
-            }
-        }
+	protected disableButtons(components: any): any
+	{
+		for (const element of components) {
+			for (const component of element.components) {
+				component.data.disabled = true;
+			}
+		}
 
-        return components;
-    }
+		return components;
+	}
 }
