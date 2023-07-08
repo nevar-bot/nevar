232d2d0e0ef30895f849d8813f431abca827621f
Jonas <p429344@gmail.com>
2023-07-09 00:23:40 +0200

    :sparkles: add: stats command

--- src/commands/misc/serverinfo.ts
+++ src/commands/misc/serverinfo.ts
@@ -3,60 +3,64 @@ import BaseClient from "@structures/BaseClient";
 import { SlashCommandBuilder, EmbedBuilder, ChannelType } from "discord.js";
 import moment from "moment";
 
-export default class ServerinfoCommand extends BaseCommand {
-    public constructor(client: BaseClient) {
-        super(client, {
-            name: "serverinfo",
-            description: "Zeigt allgemeine Informationen über den Server an",
-            cooldown: 1000,
-            dirname: __dirname,
-            slashCommand: {
-                addCommand: true,
-                data: new SlashCommandBuilder()
-            }
-        });
-    }
+export default class ServerinfoCommand extends BaseCommand
+{
+	public constructor(client: BaseClient)
+	{
+		super(client, {
+			name: "serverinfo",
+			description: "Zeigt allgemeine Informationen über den Server an",
+			cooldown: 1000,
+			dirname: __dirname,
+			slashCommand: {
+				addCommand: true,
+				data: new SlashCommandBuilder()
+			}
+		});
+	}
 
-    private interaction: any;
+	private interaction: any;
 
-    public async dispatch(interaction: any, data: any): Promise<void>{
-        this.interaction = interaction;
-        await this.showServerInfo();
-    }
+	public async dispatch(interaction: any, data: any): Promise<void>
+	{
+		this.interaction = interaction;
+		await this.showServerInfo();
+	}
 
-    private async showServerInfo(): Promise<void>{
-        const name: string = this.interaction.guild.name;
-        const id: string = this.interaction.guild.id;
-        const owner: any = await this.interaction.guild.fetchOwner();
-        const memberCount: number = this.interaction.guild.memberCount;
-        const channelCount: number = this.interaction.guild.channels.cache.size;
-        await this.interaction.guild.channels.fetch().catch((): void => {});
-        const textCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildText || c.type === ChannelType.GuildAnnouncement).size;
-        const voiceCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildVoice).size;
-        const forumCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildForum).size;
-        const categoryCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildCategory).size;
-        const threadCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildPublicThread).size;
-        const createdAt: string = moment(this.interaction.guild.createdTimestamp).format("DD.MM.YYYY HH:mm");
-        const createdAgo: string = this.client.utils.getRelativeTime(this.interaction.guild.createdTimestamp);
+	private async showServerInfo(): Promise<void>
+	{
+		const name: string = this.interaction.guild.name;
+		const id: string = this.interaction.guild.id;
+		const owner: any = await this.interaction.guild.fetchOwner();
+		const memberCount: number = this.interaction.guild.memberCount;
+		const channelCount: number = this.interaction.guild.channels.cache.size;
+		await this.interaction.guild.channels.fetch().catch((): void => { });
+		const textCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildText || c.type === ChannelType.GuildAnnouncement).size;
+		const voiceCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildVoice).size;
+		const forumCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildForum).size;
+		const categoryCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildCategory).size;
+		const threadCount: number = this.interaction.guild.channels.cache.filter((c: any): boolean => c.type === ChannelType.GuildPublicThread).size;
+		const createdAt: string = moment(this.interaction.guild.createdTimestamp).format("DD.MM.YYYY HH:mm");
+		const createdAgo: string = this.client.utils.getRelativeTime(this.interaction.guild.createdTimestamp);
 
-        const text: string =
-            " Name: **" + name + "**\n" +
-            this.client.emotes.id + " ID: **" + id + "**\n" +
-            this.client.emotes.crown + " Eigentümer: **" + owner.user.tag + "**\n" +
-            this.client.emotes.users + " Mitglieder: **" + memberCount + "**\n\n" +
-            this.client.emotes.list + " Channel: **" + channelCount + "**\n" +
-            this.client.emotes.folder + " davon Kategorien: **" + categoryCount + "**\n" +
-            this.client.emotes.channel + " davon Text: **" + textCount + "**\n" +
-            this.client.emotes.voice + " davon Sprache: **" + voiceCount + "**\n" +
-            this.client.emotes.thread + " davon Threads: **" + threadCount + "**\n" +
-            this.client.emotes.forum + " davon Foren: **" + forumCount + "**\n\n" +
-            this.client.emotes.calendar + " Erstellt am: **" + createdAt + "**\n" +
-            this.client.emotes.reminder + " Erstellt vor: **" + createdAgo + "**";
+		const text: string =
+			" Name: **" + name + "**\n" +
+			this.client.emotes.id + " ID: **" + id + "**\n" +
+			this.client.emotes.crown + " Eigentümer: **" + owner.user.tag + "**\n" +
+			this.client.emotes.users + " Mitglieder: **" + memberCount + "**\n\n" +
+			this.client.emotes.list + " Channel: **" + channelCount + "**\n" +
+			this.client.emotes.folder + " davon Kategorien: **" + categoryCount + "**\n" +
+			this.client.emotes.channel + " davon Text: **" + textCount + "**\n" +
+			this.client.emotes.voice + " davon Sprache: **" + voiceCount + "**\n" +
+			this.client.emotes.thread + " davon Threads: **" + threadCount + "**\n" +
+			this.client.emotes.forum + " davon Foren: **" + forumCount + "**\n\n" +
+			this.client.emotes.calendar + " Erstellt am: **" + createdAt + "**\n" +
+			this.client.emotes.reminder + " Erstellt vor: **" + createdAgo + "**";
 
-        const serverInfoEmbed: EmbedBuilder = this.client.createEmbed("{0}", "discord", "normal", text);
+		const serverInfoEmbed: EmbedBuilder = this.client.createEmbed("{0}", "discord", "normal", text);
 
-        serverInfoEmbed.setTitle(this.client.emotes.shine + " Informationen zu " + this.interaction.guild.name);
-        serverInfoEmbed.setThumbnail(this.interaction.guild.iconURL({dynamic: true}));
-        return this.interaction.followUp({ embeds: [serverInfoEmbed] });
-    }
+		serverInfoEmbed.setTitle(this.client.emotes.shine + " Informationen zu " + this.interaction.guild.name);
+		serverInfoEmbed.setThumbnail(this.interaction.guild.iconURL({ dynamic: true }));
+		return this.interaction.followUp({ embeds: [serverInfoEmbed] });
+	}
 }
---
+++ src/commands/misc/stats.ts
@@ -0,0 +1,88 @@
+import BaseClient from "@structures/BaseClient";
+import BaseCommand from "@structures/BaseCommand";
+import moment from "moment";
+import fs from "fs";
+import mongoose from "mongoose";
+import { SlashCommandBuilder, EmbedBuilder } from "@discordjs/builders";
+
+export default class StatsCommand extends BaseCommand
+{
+	public constructor(client: BaseClient)
+	{
+		super(client, {
+			name: "stats",
+			description: "Zeigt allgemeine Statistiken über den Bot an",
+			cooldown: 1000,
+			dirname: __dirname,
+			slashCommand: {
+				addCommand: true,
+				data: new SlashCommandBuilder()
+			}
+		});
+	}
+
+	private interaction: any;
+
+	public async dispatch(interaction: any, data: any): Promise<void>
+	{
+		this.interaction = interaction;
+		await this.sendStats();
+	}
+
+	private async sendStats(): Promise<void>
+	{
+		const staffsdata: any = (await (await mongoose.connection.db.collection("users")).find({ "staff.state": true }).toArray())
+		const head_staffs: any[] = [];
+		const staffs: any[] = [];
+
+		for (let ownerId of this.client.config.general["OWNER_IDS"]) {
+			const headStaff: any = await this.client.users.fetch(ownerId).catch((): void => { });
+			head_staffs.push(headStaff.username);
+		}
+
+		for (let userdata of staffsdata) {
+			const user: any = await this.client.users.fetch(userdata.id).catch(() => { });
+			if (userdata.staff.role === "head-staff") {
+				if (!head_staffs.includes(user.username)) head_staffs.push(user.username);
+			} else if (userdata.staff.role === "staff") {
+				if (!head_staffs.includes(user.username) && !staffs.includes(user.username)) staffs.push(user.username);
+			}
+		}
+
+
+		// @ts-ignore - Object is possibly 'null'
+		const uptime: any = this.client.utils.getRelativeTime(Date.now() - this.client.uptime);
+		const serverCount: number = this.client.guilds.cache.size;
+		const voteCount = JSON.parse(fs.readFileSync('./assets/votes.json').toString())[moment().format("MMMM").toLowerCase()] || 0;
+		const userCount: number = this.client.guilds.cache.reduce((sum: number, guild: any) => sum + (guild.available ? guild.memberCount : 0), 0);
+		const channelCount: number = this.client.channels.cache.size;
+		const commandCount: number = this.client.commands.filter((cmd) => !cmd.conf.ownerOnly && !cmd.conf.staffOnly).size;
+		const executedCommands: number = (await (await mongoose.connection.db.collection("logs").find({})).toArray()).length;
+		const packageJson: any = require("@root/package.json");
+		const botVersion: any = packageJson.version;
+		const nodeVer: string = process.version.replace("v", "");
+		const djsV: string = require("discord.js").version;
+		const date: Date = new Date(Date.now());
+		let month: string = date.toLocaleString('de-DE', { month: "long" });
+		month = month.charAt(0).toUpperCase() + month.slice(1);
+
+		const text: string =
+			this.client.emotes.users + " Staffs: **\n" + this.client.emotes.shine + " " + head_staffs.join("\n" + this.client.emotes.shine + " ") + "\n" + this.client.emotes.shine2 + " " + staffs.join("\n" + this.client.emotes.shine2 + " ") + "**\n\n" +
+			this.client.emotes.rocket + " Server: **" + this.client.format(serverCount) + "**\n" +
+			this.client.emotes.users + " Nutzer: **" + this.client.format(userCount) + "**\n" +
+			this.client.emotes.channel + " Channel: **" + this.client.format(channelCount) + "**\n\n" +
+			this.client.emotes.reminder + " Uptime: **" + uptime + "**\n" +
+			this.client.emotes.shine + " Votes im " + month + ": **" + this.client.format(voteCount) + "**\n\n" +
+			this.client.emotes.slashcommand + " Befehle: **" + commandCount + "**\n" +
+			this.client.emotes.loading + " Befehle ausgeführt: **" + this.client.format(executedCommands) + "**\n\n" +
+			this.client.emotes.code + " Bot-Version: **" + botVersion + "**\n" +
+			this.client.emotes.discordjs + " Discord.js-Version: **" + djsV + "**\n" +
+			this.client.emotes.javascript + " NodeJS-Version: **" + nodeVer + "**";
+
+		const statsEmbed: EmbedBuilder = this.client.createEmbed(text, null, "normal");
+		statsEmbed.setThumbnail(this.client.user!.displayAvatarURL());
+		statsEmbed.setTitle("Statistiken zu " + this.client.user!.username);
+
+		return this.interaction.followUp({ embeds: [statsEmbed] });
+	}
+}
