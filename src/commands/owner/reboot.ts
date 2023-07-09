2dcc400d22e64cb77706c7ec4b3e0a5a8ddfcabe
Jonas <p429344@gmail.com>
2023-07-09 14:51:30 +0200

    :sparkles: add: reboot command

---
+++ src/commands/owner/reboot.ts
@@ -0,0 +1,35 @@
+import BaseCommand from "@structures/BaseCommand";
+import BaseClient from "@structures/BaseClient";
+import { EmbedBuilder } from "discord.js";
+
+export default class RebootCommand extends BaseCommand
+{
+	public constructor(client: BaseClient)
+	{
+		super(client, {
+			name: "reboot",
+			description: "Startet den Bot neu",
+			ownerOnly: true,
+			dirname: __dirname,
+			slashCommand: {
+				addCommand: false,
+				data: null
+			}
+		});
+	}
+
+	private message: any;
+
+	public async dispatch(message: any, args: any[], data: any): Promise<void>
+	{
+		this.message = message;
+		await this.reboot();
+	}
+
+	private async reboot(): Promise<void>
+	{
+		const rebootEmbed: EmbedBuilder = this.client.createEmbed("Der Bot wird neu gestartet...", "warning", "warning",);
+		await this.message.reply({ embeds: [rebootEmbed] });
+		process.exit(1);
+	}
+}
