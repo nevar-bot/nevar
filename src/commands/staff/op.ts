aa8c6c4333f1cb814181e226068c89aac103e9e4
Jonas <p429344@gmail.com>
2023-07-09 16:49:12 +0200

    :sparkles: add: partners command

--- src/commands/staff/op.ts
+++ src/commands/staff/op.ts
@@ -1,29 +1,33 @@
 import BaseCommand from "@structures/BaseCommand";
 import BaseClient from "@structures/BaseClient";
 
-export default class OpCommand extends BaseCommand {
-    public constructor(client: BaseClient){
-        super(client, {
-            name: "op",
-            description: "Krasser OP Command",
-            staffOnly: true,
-            dirname: __dirname,
-            slashCommand: {
-                addCommand: false,
-                data: null
-            }
-        });
-    }
+export default class OpCommand extends BaseCommand
+{
+	public constructor(client: BaseClient)
+	{
+		super(client, {
+			name: "op",
+			description: "Krasser OP Command",
+			staffOnly: true,
+			dirname: __dirname,
+			slashCommand: {
+				addCommand: false,
+				data: null
+			}
+		});
+	}
 
-    private message: any;
+	private message: any;
 
-    public async dispatch(message: any, args: any[], data: any): Promise<void> {
-        this.message = message;
-        await this.op(args.join(" "));
-    }
+	public async dispatch(message: any, args: any[], data: any): Promise<void>
+	{
+		this.message = message;
+		await this.op(args.join(" "));
+	}
 
-    private async op(user: string): Promise<void> {
-        const member: any = await this.message.guild.resolveMember(user) || this.message.member;
-        return this.message.reply({ content: "*Made " + member.user.username + " a server operator*" });
-    }
+	private async op(user: string): Promise<void>
+	{
+		const member: any = await this.message.guild.resolveMember(user) || this.message.member;
+		return this.message.reply({ content: "*Made " + member.user.username + " a server operator*" });
+	}
 }
