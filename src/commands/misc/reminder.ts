60b9af0e86bc69dbb48f72a865feb2b901e8677a
Jonas <p429344@gmail.com>
2023-11-07 12:14:16 +0100

    translated reminder command #3

--- src/commands/misc/reminder.ts
+++ src/commands/misc/reminder.ts
@@ -9,7 +9,10 @@ export default class ReminderCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
 			name: "reminder",
-			description: "Verwaltet deine Erinnerungen",
+			description: "Manages your reminders",
+			localizedDescriptions: {
+				de: "Verwaltet deine Reminder",
+			},
 			cooldown: 2 * 1000,
 			dirname: __dirname,
 			slashCommand: {
@@ -17,34 +20,73 @@ export default class ReminderCommand extends BaseCommand {
 				data: new SlashCommandBuilder()
 					.addStringOption((option: any) =>
 						option
-							.setName("aktion")
-							.setDescription("Wähle eine Aktion")
+							.setName("action")
+							.setNameLocalizations({
+								de: "aktion"
+							})
+							.setDescription("Choose an action")
+							.setDescriptionLocalizations({
+								de: "Wähle eine Aktion"
+							})
 							.setRequired(true)
-							.addChoices({ name: "erstellen", value: "add" }, { name: "löschen", value: "delete" }, { name: "liste", value: "list" })
+							.addChoices(
+								{
+									name: "add",
+									name_localizations: {
+										de: "erstellen"
+									},
+									value: "add"
+								},
+								{
+									name: "delete",
+									name_localizations: {
+										de: "löschen"
+									},
+									value: "delete"
+								},
+								{
+									name: "list",
+									name_localizations: {
+										de: "liste"
+									},
+									value: "list"
+								})
 					)
 					.addStringOption((option: any) =>
 						option
 							.setName("name")
-							.setDescription("Woran soll ich dich erinnern? (beim löschen: Name der Erinnerung)")
+							.setDescription("What do you want me to remind you of? (when deleting: name of the memory)")
+							.setDescriptionLocalizations({
+								de: "Woran soll ich dich erinnern? (beim löschen: Name der Erinnerung)"
+							})
 							.setRequired(false)
 							.setMaxLength(500)
 					)
 					.addStringOption((option: any) =>
-						option.setName("dauer").setDescription("Wann soll ich dich erinnern? (z.B. 1h, 1w, 1w, 1h 30m)").setRequired(false)
+						option
+							.setName("duration")
+							.setNameLocalizations({
+								de: "dauer"
+							})
+							.setDescription("When should I remind you? (e.g. 1h, 1w, 1w, 1h 30m)")
+							.setDescriptionLocalizations({
+								de: "Wann soll ich dich erinnern? (z.B. 1h, 1w, 1w, 1h 30m)"
+							})
+							.setRequired(false)
 					)
 			}
 		});
 	}
 
-	private interaction: any;
 
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
+		this.guild = interaction.guild;
 
-		const action = interaction.options.getString("aktion");
+		const action = interaction.options.getString("action");
 		switch (action) {
 			case "add":
-				await this.addReminder(interaction.options.getString("name"), interaction.options.getString("dauer"), data);
+				await this.addReminder(interaction.options.getString("name"), interaction.options.getString("duration"), data);
 				break;
 			case "delete":
 				await this.deleteReminder(interaction.options.getString("name"), data);
@@ -57,7 +99,7 @@ export default class ReminderCommand extends BaseCommand {
 
 	private async addReminder(name: string, dauer: string, data: any): Promise<void> {
 		if (!name || !dauer || !ms(dauer)) {
-			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed("Du musst einen Namen und eine Dauer angeben.", "error", "error");
+			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed(this.translate("errors:missingNameOrDuration"), "error", "error");
 			return this.interaction.followUp({ embeds: [invalidOptionsEmbed] });
 		}
 
@@ -73,18 +115,18 @@ export default class ReminderCommand extends BaseCommand {
 		await data.member.save();
 		this.client.databaseCache.reminders.set(this.interaction.member.user.id + this.interaction.guild.id, data.member);
 
-		const successEmbed: EmbedBuilder = this.client.createEmbed("In {0} werde ich dich erinnern.", "success", "success", ms(ms(dauer)));
+		const successEmbed: EmbedBuilder = this.client.createEmbed(this.translate("created", { duration: ms(ms(dauer))}), "success", "success");
 		return this.interaction.followUp({ embeds: [successEmbed] });
 	}
 
 	private async deleteReminder(name: string, data: any): Promise<void> {
 		if (!name) {
-			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed("Du musst einen Namen angeben.", "error", "error");
+			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed(this.translate("errors:missingName"), "error", "error");
 			return this.interaction.followUp({ embeds: [invalidOptionsEmbed] });
 		}
 		const reminder: any = data.member.reminders.find((r: any): boolean => r.reason === name);
 		if (!reminder) {
-			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed("Mit dem Namen hab ich keine Erinnerung gefunden.", "error", "error");
+			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed(this.translate("errors:noReminderWithThisName"), "error", "error");
 			return this.interaction.followUp({ embeds: [invalidOptionsEmbed] });
 		}
 
@@ -92,7 +134,7 @@ export default class ReminderCommand extends BaseCommand {
 		data.member.markModified("reminders");
 		await data.member.save();
 
-		const successEmbed: EmbedBuilder = this.client.createEmbed("Die Erinnerung wurde gelöscht.", "success", "success");
+		const successEmbed: EmbedBuilder = this.client.createEmbed(this.translate("deleted"), "success", "success");
 		return this.interaction.followUp({ embeds: [successEmbed] });
 	}
 
@@ -106,19 +148,19 @@ export default class ReminderCommand extends BaseCommand {
 				reminder.reason +
 				"\n" +
 				this.client.emotes.arrow +
-				" Erstellt am: " +
+				" " + this.translate("createdAt") + " " +
 				this.client.utils.getDiscordTimestamp(reminder.startDate, "f") +
 				"\n" +
 				this.client.emotes.arrow +
-				" Endet am: " +
+				" " + this.translate("endsAt") + " " +
 				this.client.utils.getDiscordTimestamp(reminder.endDate, "f") +
 				"\n" +
 				this.client.emotes.arrow +
-				" Endet in: " +
+				" " + this.translate("endsIn") + " " +
 				this.client.utils.getDiscordTimestamp(reminder.endDate, "R");
 			reminders.push(text);
 		}
 
-		await this.client.utils.sendPaginatedEmbed(this.interaction, 5, reminders, "Erinnerungen", "Du hast keine Erinnerungen erstellt", null);
+		await this.client.utils.sendPaginatedEmbed(this.interaction, 5, reminders, this.translate("reminders"), this.translate("errors:noReminders"), null);
 	}
 }
