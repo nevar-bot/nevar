c396a8eae7d0f12caacca962b90bbf9dc7079eea
Jonas <p429344@gmail.com>
2024-01-10 23:14:54 +0100

    localized & revised servericon command #3 #19

--- src/commands/misc/servericon.ts
+++ src/commands/misc/servericon.ts
@@ -6,7 +6,10 @@ export default class ServericonCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
 			name: "servericon",
-			description: "Sendet das Icon des Servers",
+			description: "Sends the icon of the server",
+			localizedDescriptions: {
+				de: "Sendet das Icon des Servers",
+			},
 			cooldown: 1000,
 			dirname: __dirname,
 			slashCommand: {
@@ -16,41 +19,42 @@ export default class ServericonCommand extends BaseCommand {
 		});
 	}
 
-	private interaction: any;
 
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
+		this.guild = interaction.guild;
 		await this.showServerIcon();
 	}
 
-	private async showServerIcon(): Promise<void> {
-		const x64: any = this.interaction.guild.iconURL({
+	private async showServerIcon(): Promise<any> {
+		const x64: any = this.interaction.guild!.iconURL({
 			extension: "png",
 			size: 64,
 		});
-		const x128: any = this.interaction.guild.iconURL({
+		const x128: any = this.interaction.guild!.iconURL({
 			extension: "png",
 			size: 128,
 		});
-		const x256: any = this.interaction.guild.iconURL({
+		const x256: any = this.interaction.guild!.iconURL({
 			extension: "png",
 			size: 256,
 		});
-		const x512: any = this.interaction.guild.iconURL({
+		const x512: any = this.interaction.guild!.iconURL({
 			extension: "png",
 			size: 512,
 		});
-		const x1024: any = this.interaction.guild.iconURL({
+		const x1024: any = this.interaction.guild!.iconURL({
 			extension: "png",
 			size: 1024,
 		});
-		const x2048: any = this.interaction.guild.iconURL({
+		const x2048: any = this.interaction.guild!.iconURL({
 			extension: "png",
 			size: 2048,
 		});
 
 		const avatarEmbed: EmbedBuilder = this.client.createEmbed(
-			"Links: [x64]({0}) • [x128]({1}) • [x256]({2}) • [x512]({3}) • [x1024]({4}) • [x2048]({5})",
+			this.translate("links") + ": " +
+			"[x64]({0}) • [x128]({1}) • [x256]({2}) • [x512]({3}) • [x1024]({4}) • [x2048]({5})",
 			null,
 			"normal",
 			x64,
@@ -60,7 +64,7 @@ export default class ServericonCommand extends BaseCommand {
 			x1024,
 			x2048,
 		);
-		avatarEmbed.setTitle("Icon von " + this.interaction.guild.name);
+		avatarEmbed.setTitle(this.translate("title", { guild: this.guild }))
 		avatarEmbed.setImage(x256);
 
 		return this.interaction.followUp({ embeds: [avatarEmbed] });
