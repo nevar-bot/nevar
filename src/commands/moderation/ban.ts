9e81a45105567e861076a856b5bb97a86e9ba706
Jonas <jonas.pape0905@gmail.com>
2024-01-15 21:40:52 +0100

    revised and localized ban command #3 #19

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -9,7 +9,10 @@ export default class BanCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
 			name: "ban",
-			description: "Bannt ein Mitglied für eine bestimmte Zeit",
+			description: "Ban members for a time specified by you",
+			localizedDescriptions: {
+				de: "Banne Mitglieder für eine von dir bestimmte Zeit"
+			},
 			memberPermissions: ["BanMembers"],
 			botPermissions: ["BanMembers"],
 			cooldown: 1000,
@@ -18,38 +21,60 @@ export default class BanCommand extends BaseCommand {
 				addCommand: true,
 				data: new SlashCommandBuilder()
 					.addUserOption((option: any) =>
-						option.setName("mitglied").setDescription("Wähle ein Mitglied").setRequired(true),
-					)
-					.addStringOption((option: any) =>
-						option.setName("grund").setDescription("Gib einen Grund an").setRequired(false),
+						option
+							.setName("member")
+							.setNameLocalizations({
+								de: "mitglied"
+							})
+							.setDescription("Choose a member")
+							.setDescriptionLocalizations({
+								de: "Wähle ein Mitglied aus"
+							})
+							.setRequired(true),
 					)
 					.addStringOption((option: any) =>
 						option
-							.setName("dauer")
-							.setDescription("Gib eine Dauer an (bspw. 1h, 1d, 1h 30m, etc.)")
+							.setName("reason")
+							.setNameLocalizations({
+								de: "grund"
+							})
+							.setDescription("Choose a reason")
+							.setDescriptionLocalizations({
+								de: "Gib einen Grund an"
+							})
+							.setRequired(false),
+					)
+					.addStringOption((option: any) =>
+						option
+							.setName("duration")
+							.setNameLocalizations({
+								de: "dauer"
+							})
+							.setDescription("Choose a duration (e.g. 1h, 1d, 1h 30m, etc.)")
+							.setDescriptionLocalizations({
+								de: "Gib eine Dauer an (bspw. 1h, 1d, 1h 30m, etc.)"
+							})
 							.setRequired(false),
 					),
 			},
 		});
 	}
 
-	private interaction: any;
-
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
 		await this.ban(
-			interaction.options.getUser("mitglied"),
-			interaction.options.getString("grund"),
-			interaction.options.getString("dauer"),
+			interaction.options.getUser("member"),
+			interaction.options.getString("reason"),
+			interaction.options.getString("duration"),
 			data,
 		);
 	}
 
-	private async ban(member: any, reason: string, duration: string, data: any): Promise<void> {
-		member = await this.interaction.guild.resolveMember(member.id);
+	private async ban(member: any, reason: string, duration: string, data: any): Promise<any> {
+		member = await this.interaction.guild!.resolveMember(member.id);
 		if (!member) {
 			const noMemberEmbed: EmbedBuilder = this.client.createEmbed(
-				"Du musst ein Mitglied angeben.",
+				this.translate("errors:missingMember"),
 				"error",
 				"error",
 			);
@@ -58,7 +83,7 @@ export default class BanCommand extends BaseCommand {
 
 		if (member.user.id === this.interaction.user.id) {
 			const selfEmbed: EmbedBuilder = this.client.createEmbed(
-				"Du kannst dich nicht selbst bannen.",
+				this.translate("errors:cantBanYourself"),
 				"error",
 				"error",
 			);
@@ -67,16 +92,16 @@ export default class BanCommand extends BaseCommand {
 
 		if (member.user.id === this.client.user!.id) {
 			const meEmbed: EmbedBuilder = this.client.createEmbed(
-				"Ich kann mich nicht selbst bannen.",
+				this.translate("errors:cantBanMyself"),
 				"error",
 				"error",
 			);
 			return this.interaction.followUp({ embeds: [meEmbed] });
 		}
 
-		if (member.roles.highest.position >= this.interaction.member.roles.highest.position) {
+		if (member.roles.highest.position >= this.interaction.member!.roles.highest.position) {
 			const higherRoleEmbed: EmbedBuilder = this.client.createEmbed(
-				"Du kannst keine Mitglieder bannen, die eine höhere Rolle haben als du.",
+				this.translate("errors:cantBanHigher"),
 				"error",
 				"error",
 			);
@@ -85,7 +110,7 @@ export default class BanCommand extends BaseCommand {
 
 		if (!member.bannable) {
 			const cantBanEmbed: EmbedBuilder = this.client.createEmbed(
-				"Ich kann dieses Mitglied nicht bannen.",
+				this.translate("errors:cantBan"),
 				"error",
 				"error",
 			);
@@ -94,7 +119,7 @@ export default class BanCommand extends BaseCommand {
 
 		if (duration && !ms(duration)) {
 			const invalidDurationEmbed: EmbedBuilder = this.client.createEmbed(
-				"Du hast eine ungültige Dauer angegeben.",
+				this.translate("errors:invalidDuration"),
 				"error",
 				"error",
 			);
@@ -105,13 +130,13 @@ export default class BanCommand extends BaseCommand {
 
 		const ban: any = {
 			victim: member,
-			reason: reason || "Kein Grund angegeben",
+			reason: reason || this.translate("noReasonSpecified"),
 			duration: duration ? ms(duration) : 200 * 60 * 60 * 24 * 365 * 1000,
 		};
 
 		let relativeTime: string = this.client.utils.getDiscordTimestamp(Date.now() + ban.duration, "R");
 		if (ban.duration === 200 * 60 * 60 * 24 * 365 * 1000) {
-			relativeTime = "Permanent";
+			relativeTime = this.translate("permanent");
 		}
 		let unbanDate: string = moment(Date.now() + ban.duration).format("DD.MM.YYYY, HH:mm");
 		if (ban.duration === 200 * 60 * 60 * 24 * 365 * 1000) {
@@ -119,13 +144,12 @@ export default class BanCommand extends BaseCommand {
 		}
 
 		const areYouSureEmbed: EmbedBuilder = this.client.createEmbed(
-			"Bist du dir sicher, dass du {0} bannen möchtest?",
+			this.translate("confirmation", { user: member.toString() }),
 			"arrow",
 			"warning",
-			member.user.username,
 		);
-		const buttonYes: ButtonBuilder = this.client.createButton("confirm", "Ja", "Secondary", "success");
-		const buttonNo: ButtonBuilder = this.client.createButton("decline", "Nein", "Secondary", "error");
+		const buttonYes: ButtonBuilder = this.client.createButton("confirm", this.translate("basics:yes", undefined, true), "Secondary", "success");
+		const buttonNo: ButtonBuilder = this.client.createButton("decline", this.translate("basics:no", undefined, true), "Secondary", "error");
 		const buttonRow: any = this.client.createMessageComponentsRow(buttonYes, buttonNo);
 
 		const confirmationAskMessage: any = await this.interaction.followUp({
@@ -145,45 +169,45 @@ export default class BanCommand extends BaseCommand {
 				case "confirm":
 					const privateText: string =
 						"### " +
-						this.client.emotes.ban +
-						" Du wurdest auf {0} gebannt.\n\n" +
-						this.client.emotes.arrow +
-						" Begründung: " +
+						this.client.emotes.ban + " " +
+						this.translate("privateMessage:title", { guild: this.interaction.guild!.name }) + "\n\n" +
+						this.client.emotes.arrow + " " +
+						this.translate("reason") + ": " +
 						ban.reason +
 						"\n" +
-						this.client.emotes.arrow +
-						" Dauer: " +
+						this.client.emotes.arrow + " " +
+						this.translate("duration") + ": " +
 						relativeTime +
 						"\n" +
-						this.client.emotes.arrow +
-						" Moderator: " +
+						this.client.emotes.arrow + " " +
+						this.translate("moderator") + ": " +
 						this.interaction.user.username +
 						"\n" +
-						this.client.emotes.arrow +
-						" Unban am: " +
+						this.client.emotes.arrow + " " +
+						this.translate("unbanAt") + ": " +
 						unbanDate;
+
 					const privateBanEmbed: EmbedBuilder = this.client.createEmbed(
 						privateText,
 						null,
-						"error",
-						this.interaction.guild.name,
+						"error"
 					);
 					const privateMessage = await ban.victim.send({ embeds: [privateBanEmbed] }).catch((): void => {});
 					try {
 						await ban.victim.ban({
 							reason:
-								"BAN - Dauer: " +
-								relativeTime +
-								" | Begründung: " +
-								ban.reason +
-								" | Moderator: " +
-								this.interaction.user.username +
-								" | Unban am: " +
+								this.translate("duration") + ": " +
+								relativeTime + " | " +
+								this.translate("reason") + ": " +
+								ban.reason + " | " +
+								this.translate("moderator") + ": " +
+								this.interaction.user.username + " | " +
+								this.translate("unbanAt") + ": " +
 								unbanDate,
 						});
 						const victimData = await this.client.findOrCreateMember(
 							ban.victim.user.id,
-							this.interaction.guild.id,
+							this.interaction.guild!.id,
 						);
 
 						victimData.banned = {
@@ -200,28 +224,28 @@ export default class BanCommand extends BaseCommand {
 						victimData.markModified("banned");
 						await victimData.save();
 						this.client.databaseCache.bannedUsers.set(
-							ban.victim.user.id + this.interaction.guild.id,
+							ban.victim.user.id + this.interaction.guild!.id,
 							victimData,
 						);
 
 						const publicText: string =
 							"### " +
-							this.client.emotes.ban +
-							" {0} wurde gebannt.\n\n" +
-							this.client.emotes.arrow +
-							" Begründung: " +
+							this.client.emotes.ban + " " +
+							this.translate("publicMessage:title", { user: ban.victim.user.username }) + "\n\n" +
+							this.client.emotes.arrow + " " +
+							this.translate("reason") + ": " +
 							ban.reason +
 							"\n" +
-							this.client.emotes.arrow +
-							" Dauer: " +
+							this.client.emotes.arrow + " " +
+							this.translate("duration") + ": " +
 							relativeTime +
 							"\n" +
-							this.client.emotes.arrow +
-							" Moderator: " +
+							this.client.emotes.arrow + " " +
+							this.translate("moderator") + ": " +
 							this.interaction.user.username +
 							"\n" +
-							this.client.emotes.arrow +
-							" Unban am: " +
+							this.client.emotes.arrow + " " +
+							this.translate("unbanAt") + ": " +
 							unbanDate;
 						const publicBanEmbed: EmbedBuilder = this.client.createEmbed(
 							publicText,
@@ -238,10 +262,9 @@ export default class BanCommand extends BaseCommand {
 						//console.log(exc);
 						privateMessage.delete().catch((): void => {});
 						const cantBanEmbed: EmbedBuilder = this.client.createEmbed(
-							"Ich konnte {0} nicht bannen.",
+							this.translate("errors:banFailed", { user: ban.victim.user.toString() }),
 							"error",
 							"error",
-							ban.victim.user.username,
 						);
 						await clicked.update({
 							embeds: [cantBanEmbed],
@@ -251,10 +274,9 @@ export default class BanCommand extends BaseCommand {
 					break;
 				case "decline":
 					const declineEmbed: EmbedBuilder = this.client.createEmbed(
-						"{0} wurde nicht gebannt.",
+						this.translate("banRejected", { user: ban.victim.user.toString() }),
 						"error",
 						"error",
-						ban.victim.user.username,
 					);
 					await clicked.update({
 						embeds: [declineEmbed],
