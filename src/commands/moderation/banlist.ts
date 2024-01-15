c3b5a1837965e490f907b5f49bfc48a51a412006
Jonas <jonas.pape0905@gmail.com>
2024-01-15 21:49:09 +0100

    revised and localized banlist command #3 #19

--- src/commands/moderation/banlist.ts
+++ src/commands/moderation/banlist.ts
@@ -7,7 +7,10 @@ export default class BanlistCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
 			name: "banlist",
-			description: "Listet alle gebannten Mitglieder",
+			description: "Lists all banned members",
+			localizedDescriptions: {
+				de: "Listet alle gebannten Mitglieder",
+			},
 			memberPermissions: ["BanMembers"],
 			botPermissions: ["BanMembers"],
 			cooldown: 1000,
@@ -19,19 +22,19 @@ export default class BanlistCommand extends BaseCommand {
 		});
 	}
 
-	private interaction: any;
 
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
+		this.guild = interaction.guild;
 		await this.showBanList();
 	}
 
-	private async showBanList(): Promise<void> {
+	private async showBanList(): Promise<any> {
 		const bannedUsers: any[] = [];
-		const bans: any = await this.interaction.guild.bans.fetch().catch((): void => {});
+		const bans: any = await this.interaction.guild!.bans.fetch().catch((): void => {});
 		for (const ban of bans) {
-			const memberData: any = await this.client.findOrCreateMember(ban[1].user.id, this.interaction.guild.id);
-			if (memberData.banned.state) {
+			const memberData: any = await this.client.findOrCreateMember(ban[1].user.id, this.interaction.guild!.id);
+			if (memberData?.banned.state) {
 				// Mit Nevar gebannt
 				const duration: string =
 					memberData.banned.duration === 200 * 60 * 60 * 24 * 365 * 1000
@@ -50,24 +53,24 @@ export default class BanlistCommand extends BaseCommand {
 					" " +
 					ban[1].user.username +
 					"\n" +
-					this.client.emotes.arrow +
-					" Begründung: " +
+					this.client.emotes.arrow + " " +
+					this.translate("reason") + ": " +
 					memberData.banned.reason +
 					"\n" +
-					this.client.emotes.arrow +
-					" Moderator: " +
+					this.client.emotes.arrow + " " +
+					this.translate("moderator") + ": " +
 					(moderator ? moderator.username : memberData.banned.moderator.name) +
 					"\n" +
-					this.client.emotes.arrow +
-					" Dauer: " +
+					this.client.emotes.arrow + " " +
+					this.translate("duration") + ": " +
 					duration +
 					"\n" +
-					this.client.emotes.arrow +
-					" Gebannt am: " +
+					this.client.emotes.arrow + " " +
+					this.translate("bannedAt") + ": " +
 					moment(memberData.banned.bannedAt).format("DD.MM.YYYY, HH:mm") +
 					"\n" +
-					this.client.emotes.arrow +
-					" Gebannt bis: " +
+					this.client.emotes.arrow + " " +
+					this.translate("bannedUntil") + ": " +
 					bannedUntil +
 					"\n";
 				bannedUsers.push(text);
@@ -79,9 +82,9 @@ export default class BanlistCommand extends BaseCommand {
 					" " +
 					ban[1].user.username +
 					"\n" +
-					this.client.emotes.arrow +
-					" Begründung: " +
-					ban[1].reason +
+					this.client.emotes.arrow + " " +
+					this.translate("reason") + ": " +
+					(ban[1].reason ? ban[1].reason : this.translate("noReasonSpecified")) +
 					"\n";
 				bannedUsers.push(text);
 			}
@@ -90,9 +93,8 @@ export default class BanlistCommand extends BaseCommand {
 			this.interaction,
 			3,
 			bannedUsers,
-			"Gebannte Nutzer/-innen",
-			"Es sind keine Nutzer gebannt",
-			null,
+			this.translate("list:title"),
+			this.translate("list:empty")
 		);
 	}
 }
