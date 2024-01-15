80ae751b0ffbaca081f432303ace1c519883b084
Jonas <jonas.pape0905@gmail.com>
2024-01-15 21:20:53 +0100

    revised and localized xpfor command #3 #19

--- src/commands/misc/xpfor.ts
+++ src/commands/misc/xpfor.ts
@@ -6,26 +6,39 @@ export default class XpForCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
 			name: "xpfor",
-			description: "Zeigt, wieviel XP für ein bestimmtes Level benötigt wird",
+			description: "Erfahre, wieviel XP du für ein bestimmtes Level benötigst",
+			localizedDescription: {
+				de: "Erfahre, wieviel XP du für ein bestimmtes Level benötigst",
+			},
 			cooldown: 1000,
 			dirname: __dirname,
 			slashCommand: {
 				addCommand: true,
 				data: new SlashCommandBuilder().addIntegerOption((option: any) =>
-					option.setName("level").setDescription("Gib das Level an").setMinValue(1).setRequired(true),
+					option
+						.setName("level")
+						.setNameLocalizations({
+							de: "level"
+						})
+						.setDescription("Enter the level")
+						.setDescriptionLocalizations({
+							de: "Gib das Level an"
+						})
+						.setMinValue(1)
+						.setRequired(true),
 				),
 			},
 		});
 	}
 
-	private interaction: any;
 
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
+		this.guild = interaction.guild;
 		await this.sendXpFor(interaction.options.getInteger("level"), data);
 	}
 
-	private async sendXpFor(level: number, data: any): Promise<void> {
+	private async sendXpFor(level: number, data: any): Promise<any> {
 		function secondsToTime(secs: number): string {
 			secs = Math.round(secs);
 			const hours: number = Math.floor(secs / (60 * 60));
@@ -43,32 +56,11 @@ export default class XpForCommand extends BaseCommand {
 		const maxXp = data.guild.settings.levels.xp.max;
 		const averageXp: number = Math.round((minXp + maxXp) / 2);
 		const neededXp: string = this.client.format(this.client.levels.xpFor(level));
-		const timeoutLengthInSeconds: number = 15;
-		const neededTime: string = secondsToTime(
-			(this.client.levels.xpFor(level) / averageXp) * timeoutLengthInSeconds,
-		);
+
 		const neededMessages: string = this.client.format(Math.round(this.client.levels.xpFor(level) / averageXp));
 
 		const text: string =
-			"Um Level **" +
-			level +
-			"** zu erreichen, werden **" +
-			neededXp +
-			" XP** benötigt.\n" +
-			this.client.emotes.arrow +
-			" Auf diesem Server werden " +
-			minXp +
-			" bis " +
-			maxXp +
-			" XP pro Nachricht vergeben, der Durchschnitt liegt bei " +
-			averageXp +
-			" XP pro Nachricht.\n" +
-			this.client.emotes.arrow +
-			" Bei einem Timeout von 15 Sekunden, entspricht dies etwa **" +
-			neededMessages +
-			" Nachrichten**, und einem Zeitaufwand von **" +
-			neededTime +
-			"**.";
+			this.translate("needed", { level, neededXp, neededMessages });
 
 		const xpForEmbed: EmbedBuilder = this.client.createEmbed("{0}", "arrow", "normal", text);
 		return this.interaction.followUp({ embeds: [xpForEmbed] });
