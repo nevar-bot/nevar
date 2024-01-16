7ae1b7531a757e9f04c822821701f429f78b16d3
Jonas <jonas.pape0905@gmail.com>
2024-01-16 17:17:26 +0100

    localized and revised resetwarns command #3 #19

--- src/commands/moderation/resetwarns.ts
+++ src/commands/moderation/resetwarns.ts
@@ -6,28 +6,49 @@ export default class ResetwarnsCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
 			name: "resetwarns",
-			description: "Setzt die Verwarnungen eines Mitgliedes zurück",
+			description: "Resets a member's warnings",
+			localizedDescriptions: {
+				de: "Setzt die Verwarnungen eines Mitgliedes zurück",
+			},
 			memberPermissions: ["KickMembers"],
 			cooldown: 1000,
 			dirname: __dirname,
 			slashCommand: {
 				addCommand: true,
 				data: new SlashCommandBuilder().addUserOption((option: any) =>
-					option.setName("mitglied").setDescription("Wähle ein Mitglied").setRequired(true),
+					option
+						.setName("member")
+						.setNameLocalizations({
+							de: "mitglied"
+						})
+						.setDescription("Choose a member")
+						.setDescriptionLocalizations({
+							de: "Wähle ein Mitglied"
+						})
+						.setRequired(true),
 				),
 			},
 		});
 	}
 
-	private interaction: any;
 
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
-		await this.resetWarns(interaction.options.getUser("mitglied"));
+		this.guild = interaction.guild;
+		await this.resetWarns(interaction.options.getMember("member"));
 	}
 
-	private async resetWarns(user: any): Promise<void> {
-		const memberData: any = await this.client.findOrCreateMember(user.id, this.interaction.guild.id);
+	private async resetWarns(member: any): Promise<any> {
+		if(!member){
+			const invalidOptionsEmbed: EmbedBuilder = this.client.createEmbed(
+				this.translate("basics:errors:missingMember", {}, true),
+				"error",
+				"error",
+			);
+			return this.interaction.followUp({ embeds: [invalidOptionsEmbed] });
+		}
+
+		const memberData: any = await this.client.findOrCreateMember(member.id, this.interaction.guild!.id);
 
 		memberData.warnings = {
 			count: 0,
@@ -37,23 +58,20 @@ export default class ResetwarnsCommand extends BaseCommand {
 		await memberData.save();
 
 		const logText: string =
-			"### " +
-			this.client.emotes.delete +
-			" Verwarnungen von " +
-			user.username +
-			" zurückgesetzt\n\n" +
-			this.client.emotes.user +
-			" Moderator: " +
-			this.interaction.user.username;
+			"### " + this.client.emotes.delete + " " +
+			this.translate("logTitle", { user: member.toString() }) + "\n\n" +
+			this.client.emotes.user + " " +
+			this.translate("moderator") + ": " +
+			this.interaction.member!.toString();
+
 		const logEmbed: EmbedBuilder = this.client.createEmbed(logText, null, "normal");
-		logEmbed.setThumbnail(user.displayAvatarURL());
-		await this.interaction.guild.logAction(logEmbed, "moderation");
+		logEmbed.setThumbnail(member.displayAvatarURL());
+		await this.interaction.guild!.logAction(logEmbed, "moderation");
 
 		const successEmbed: EmbedBuilder = this.client.createEmbed(
-			"Die Verwarnungen von {0} wurden zurückgesetzt.",
+			this.translate("reset", { user: member.toString() }),
 			"success",
-			"success",
-			user.username,
+			"success"
 		);
 		return this.interaction.followUp({ embeds: [successEmbed] });
 	}
